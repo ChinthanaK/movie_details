dc8cc9d5a202a90e0c1915f0f24b37ce859fcf13
ChinthanaK <chinthanakumar1999>
2025-11-03 00:13:48 +0530

    Fetch Movie on Page load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,14 +7,14 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [retrying, setRetrying] = useState(true);
+  
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback( async () => {
     try{
       setIsLoading(true);
       setError(null);
     const response = await fetch("https://swapi.info/api/films");
-    console.log(response.ok);
+    console.log(response.ok)
      if(!response.ok){
       throw new Error("Something went wrong ....Retrying")
     }
@@ -26,27 +26,19 @@ function App() {
     }
      setIsLoading(false);
     
-  }
-  function cancelHandler(){
-     setRetrying(false);
-    setError("Retrying stopped by user.");
-  }
-
+  }, []
+ 
+  )
     useEffect(() =>{
-    if (!retrying) return;
-    const interval = setInterval(() => {
       fetchMoviesHandler();
-    }, 3000);
-   
-  
-  return () => clearInterval(interval);
-  },[fetchMoviesHandler, retrying]);
+    }, [fetchMoviesHandler]);
+
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-        <button onClick={cancelHandler}>Cancel</button>
+        
       </section>
       <section>
         {isLoading && <p>Loading...</p>}
